68cbc1084a77d5246f33909627bb975f94ec8b7d
Myo Win Thein <myowin@nexlabs.co>
2020-05-09 03:30:26 +0630

    fuck it, i'm gonna commit & sleep

--- js/input.js
+++ js/input.js
@@ -9,21 +9,16 @@ window.addEventListener('load', (event) => {
     const backButton = document.getElementById('back')   
 
     // back click
-    if (backButton) {
-        backButton.addEventListener('click', function() {
-            window.location.href = 'index.html'
-        })
-    }
+    backButton.addEventListener('click', function() {
+        window.location.href = 'index.html'
+    })
 
     // save click
-    if (saveButton) {
-        saveButton.addEventListener('click', function() {
-            let values = document.getElementById('mwt').value.split('\n')
-            // console.log(values)
-            // validation
-            // loop
-                // trim, clear dots
-            // generate json ifle
-        })
-    }
+    saveButton.addEventListener('click', function() {
+        // let values = document.getElementById('mwt').value.split('\n')
+        // validation
+        // loop
+            // trim, clear dots
+        // generate json ifle
+    })
 })
